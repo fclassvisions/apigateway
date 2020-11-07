bc3c9efff745259dee3623d183f040e088a2802d
cooperj <cooperj@supakingcoopa-pc.com>
2020-11-07 09:52:07 -0600

    Changed gateway port back

--- gateway.js
+++ gateway.js
@@ -2,7 +2,7 @@ const express = require('express')
 const helmet = require('helmet')
 const app = express()
 const routes = require('./routes')
-const PORT = 3020
+const PORT = 3000
 
 app.use(express.json())
 app.use(helmet())
